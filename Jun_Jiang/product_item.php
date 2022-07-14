9cc4dec43fa0875f7311b512b698ae3ec1360aca
Junjiang1 <107907580+Junjiang1@users.noreply.github.com>
2022-07-14 11:28:30 +0800

    Add files via upload

---
+++ Jun_Jiang/product_item.php
@@ -0,0 +1,58 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+
+	<title>Product Item</title>
+
+	
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="	lib/css/styleguide.css">
+<link rel="stylesheet" href="	lib/css/gridsystem.css">
+<link rel="stylesheet" href="	css/storetheme.css">
+<link rel="stylesheet" href="	css/style1.css">
+</head>
+<body>
+	
+		
+<!-- 	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Store</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class=" nav nav-flex flex-none">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="product_list.php">Store</a></li>
+					<li><a href="#">Link 3</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+ -->
+	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Healthy Foodie</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="flex-none"></nav>
+			<nav class="nav">
+				<ul class="container display-flex">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="product list.php">Product List</a></li>
+					<li><a href="detail.php">Cart</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<div class="container">
+		<div class="card soft">
+			<h2>Product Item</h2>
+
+			<p>This is item # 1</p>
+		</div>
+	</div>
+
+</body>
+</html>
