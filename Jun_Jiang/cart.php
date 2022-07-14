2087be6b13c23cd062aa1051b975e20b29c2c6f8
Junjiang1 <107907580+Junjiang1@users.noreply.github.com>
2022-07-14 12:34:56 +0800

    Add files via upload

---
+++ Jun_Jiang/cart.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Healthy Foodie</title>
+
+     
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="	lib/css/styleguide.css">
+<link rel="stylesheet" href="	lib/css/gridsystem.css">
+<link rel="stylesheet" href="	css/storetheme.css">
+<link rel="stylesheet" href="	css/style1.css"></head>
+
+    	
+<!-- 	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Store</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class=" nav nav-flex flex-none">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="product_list.php">Store</a></li>
+					<li><a href="#">Link 3</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+ -->
+<!-- 	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Healthy Foodie</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="flex-none"></nav>
+			<nav class="nav">
+				<ul class="container display-flex">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="product list.php">Product List</a></li>
+					<li><a href="cart.php">Cart</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>   -->
+	
+	<?php include('./part/navbar.php');?>
+
+	  <div class="title">
+        <h2>Add to Cart</h2>
+    </div>
+
+     <div class="container">
+            <div class="grid gap">
+                <div class="col-xs-12 col-md-6 card soft">
+                    <div class="figure product">
+                        <img src="https://via.placeholder.com/400x400?text=product" alt="">
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-md-6 card soft">
+                  
+                    </div>
+                   
+                </div>
+            </div>
+
+            
+    </div>
+     <footer>&copy; 2022 ART_JUN JIANG</footer>
+
+</body>
+</html>
---
+++ Jun_Jiang/note/index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Healthy Foodie</title>
+
+	<link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
+	<link rel="stylesheet" type="text/css" href="css/storetheme.css">
+	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+	<link rel="stylesheet" href="css/style1.css">
+</head>
+<body>
+
+	<!-- header>h1+P -->
+	<header class="navbar">
+	<div class="container display-flex">
+		<div class="flex-none">
+		<h1>Healthy Foodie</h1>
+		</div>
+
+	<div class="flex-stretch"></div>
+	<nav class="flex-none nav">
+		<ul class="container display-flex">
+			<li><a href="#article1">Home</a></li>
+			<li><a href="#article2">Product list</a></li>
+			<li><a href="#article3">Cart</a></li>
+			
+		</ul>
+	</nav>
+	</div>
+	</header>
+
+
+	<!-- .coontainer>article#article$.article*4>h2{Article $}+div.article-body>p*3>lorem40 -->
+
+
+	<div class="title">
+		<h1>Welcome!</h1>
+	</div>
+
+	<div class="container">
+
+
+		<div class="view-window" style="background-color: darkgray;">
+			
+		</div>
+
+
+
+
+		</article>
+
+		<article id="article2" class="article">
+				<div class="flex-stretch">
+				<h2 class="company">Jun's Company, Inc.</h2>
+				</div>
+				<div class="article-body">
+				<p>We pay more and more attention to dietary health issues, such as hormones, pesticides, and food safety. The purpose of this website is to provide people with safe, healthy and delicious food through this website
+
+
+				</p>
+			</div>
+		</article>
+		</div>
+
+		<div class="container">
+			<h3 class="company">Organic Food</h3>
+            <div class="grid gap">
+                <div class="col-xs-12 col-md-4">
+                    <figure class="figure product">
+                        <img src="https://via.placeholder.com/400x400?text=product" alt="">
+                        <figcaption>
+                            <div>Product Name</div>
+                            <div>$3.99</div>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <figure class="figure product">
+                        <img src="https://via.placeholder.com/400x400?text=product" alt="">
+                        <figcaption>
+                            <div>Product Name</div>
+                            <div>$3.99</div>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <figure class="figure product">
+                        <img src="https://via.placeholder.com/400x400?text=product" alt="">
+                        <figcaption>
+                            <div>Product Name</div>
+                            <div>$3.99</div>
+                        </figcaption>
+                    </figure>
+                </div>
+
+
+            </div>
+            <div class="container">
+            	<h3 class="company">Super Food</h3>
+
+            <div class="grid gap">
+                <div class="col-xs-12 col-md-4">
+                    <figure class="figure product">
+                        <img src="https://via.placeholder.com/400x400?text=product" alt="">
+                        <figcaption>
+                            <div>Product Name</div>
+                            <div>$3.99</div>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <figure class="figure product">
+                        <img src="https://via.placeholder.com/400x400?text=product" alt="">
+                        <figcaption>
+                            <div>Product Name</div>
+                            <div>$3.99</div>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <figure class="figure product">
+                        <img src="https://via.placeholder.com/400x400?text=product" alt="">
+                        <figcaption>
+                            <div>Product Name</div>
+                            <div>$3.99</div>
+                        </figcaption>
+                    </figure>
+                </div>
+					
+			</div>
+	</div>
+
+</body>
+
+		<footer>&copy; 2022 ART_JUN JIANG</footer>
+</html>
