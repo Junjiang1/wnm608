2b26c8d17e5fc9bdbe5d875f5cbb1aa47552f381
Junjiang1 <107907580+Junjiang1@users.noreply.github.com>
2022-07-17 08:27:07 +0800

    Add files via upload

---
+++ Jun_Jiang/part/navbar.php
@@ -0,0 +1,18 @@
+
+	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Healthy Foodie</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="flex-none"></nav>
+			<nav class="nav">
+				<ul class="container display-flex">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="product_list.php">Product list</a></li>
+					<li><a href="cart.php">Cart</a></li>
+	
+				</ul>
+			</nav>
+		</div>
+	</header>
