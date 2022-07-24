427991449cad732004323d1bc1aa32d5f077040d
Junjiang1 <107907580+Junjiang1@users.noreply.github.com>
2022-07-24 12:43:06 +0800

    Delete Jun_Jiang directory

--- Jun_Jiang/part/meta.php
+++
@@ -1,9 +0,0 @@
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<base href="http://www.junjiangdesign.com/aau/wnm608/Jun_Jiang/">
-
-<link rel="stylesheet" href="lib/css/styleguide.css">
-<link rel="stylesheet" href="lib/css/gridsystem.css">
-<link rel="stylesheet" href="css/storetheme.css">
-<link rel="stylesheet" href="css/style1.css">
